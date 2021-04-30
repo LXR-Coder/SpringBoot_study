5b5e6dc24cd63b1bea155ac4792dc857b6dae9c4
jim <jimliu@etekcity.com.cn>
2021-04-30 18:12:10 +0800

    4.27

--- src/main/java/com/example/demo/IocApplication.java
+++ src/main/java/com/example/demo/IocApplication.java
@@ -2,12 +2,14 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@ServletComponentScan
 @SpringBootApplication
-public class HelloWorldApplication {
+public class IocApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HelloWorldApplication.class, args);
+		SpringApplication.run(IocApplication.class, args);
 	}
 
 }
